afc0bd90d14968a249c9b533aa81ab60c19a6a4b
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-07-04 13:23:50 -0400

    create user repository

---
+++ src/user/user.repository.ts
@@ -0,0 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+import { SignupDto } from 'src/dto/signup.dto';
+import bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getUserFromEmail(email: string) {
+    const existingUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    return existingUser;
+  }
+
+  async save(dto: SignupDto) {
+    const hashed = await bcrypt.hash(dto?.password, 10);
+
+    const user = this.userRepository.create({
+      email: dto.email,
+      password: hashed,
+    });
+
+    await this.userRepository.save(user);
+  }
+}
